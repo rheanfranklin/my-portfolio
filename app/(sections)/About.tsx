bf8a311de565ac54c2b5cdd34e2033f2a2186f38
rheanfranklin <rheannon.franklin@gmail.com>
2026-06-05 23:37:19 -0400

    feat: implementing first iteration of About page

--- app/(sections)/About.tsx
+++ app/(sections)/About.tsx
@@ -1,12 +1,37 @@
+"use client";
+
+import AnimatedSection from "../components/AnimatedSection";
+import { fadeUp } from "@/lib/animations/fade";
+import { MyStyleParagraph, MyHistoryParagraph, MyMotivationParagraph, AboutParagraph } from "@/content/about";   
+
 export default function About() {
     return (
-        <section 
-            className="
-                flex
-                min-h-screen
-                flex-col
-                items-center
-                justify-center
-        "></section>
+        <div className="container-box">
+            <AnimatedSection variants={fadeUp}>
+                {/* About Subheader */ }
+                <div className="subheader">
+                    About Me
+                </div>
+                <AboutParagraph />
+
+                {/* My Style Subsection */}
+                <div className="sectionTitle">
+                    My Style
+                </div>
+                <MyStyleParagraph />
+
+                {/* My Motivation Subsection */}
+                <div className="sectionTitle">
+                    My Motivation
+                </div>
+                <MyMotivationParagraph />
+
+                {/* My History Subsection */}
+                <div className="sectionTitle">
+                    My History
+                </div>
+                <MyHistoryParagraph />
+            </AnimatedSection>
+        </div>
     )
 }
---
+++ content/about.tsx
@@ -0,0 +1,32 @@
+
+export const AboutParagraph = () => (
+  <p className="paragraph">
+    Hello there! I'm so excited you're here! On this page you can find an indepth description
+    of who I am, both as a person and engineer, so you can know whether I'm the right fit for
+    your company.
+  </p>
+);
+
+
+export const MyStyleParagraph = () => (
+  <p className="paragraph">
+    My priorities when developing software is thorough communication
+    & documentation of implementation details to ease future efforts
+    of scaling and debugging the systems I work on.
+    <br />
+    When it comes to AI, 
+  </p>
+);
+
+export const MyMotivationParagraph = () => (
+  <p className="paragraph">
+    Test
+  </p>
+);
+
+
+export const MyHistoryParagraph = () => (
+  <p className="paragraph">
+    Test
+  </p>
+);
