87ae5606156b3516c604c8d5f76d52251f834a5d
rheanfranklin <rheannon.franklin@gmail.com>
2026-06-13 01:30:40 -0400

    feat: adding more logos

--- content/skills.tsx
+++ content/skills.tsx
@@ -1,51 +1,12 @@
-export const PythonLogo: React.ReactNode = <img
-    src="/images/icons/python-logo.svg"
-    alt="python logo"
-    className="logo-image"
-/>
-
-export const JavaLogo: React.ReactNode = <img
-    src="/images/icons/java-logo.svg"
-    alt="javascript logo"
-    className="logo-image"
-/>
-
-export const GoLangLogo: React.ReactNode = <img
-    src="/images/icons/golang-logo.svg"
-    alt="python logo"
-    className="go-logo"
-/>
-
-export const JavascriptLogo: React.ReactNode = <img
-    src="/images/icons/javascript-logo.svg"
-    alt="javascript logo"
-    className="script-logo"
-/>
-export const TypescriptLogo: React.ReactNode = <img
-    src="/images/icons/typescript-logo.svg"
-    alt="typescript logo"
-    className="script-logo"
-/>
-export const SQLLogo: React.ReactNode = <img
-    src="/images/icons/sql-logo.svg"
-    alt="SQL logo"
-    className="sql-logo"
-/>
-export const GraphQLLogo: React.ReactNode = <img
-    src="/images/icons/graphql-logo.svg"
-    alt="graphQL logo"
+export const AWSLogo: React.ReactNode = <img
+    src="/images/icons/aws-logo.png"
+    alt="aws logo"
     className="logo-image"
 />
 
 export const BashLogo: React.ReactNode = <img
     src="/images/icons/bash-logo.svg"
     alt="bash logo"
-    className="logo-image"
-/>
-
-export const HTMLLogo: React.ReactNode = <img
-    src="/images/icons/html-logo.svg"
-    alt="html logo"
     className="logo-image"
 />
 
@@ -61,15 +22,21 @@ export const CLogo: React.ReactNode = <img
     className="logo-image"
 />
 
-export const AWSLogo: React.ReactNode = <img
-    src="/images/icons/aws-logo.png"
-    alt="aws logo"
-    className="logo-image"
-/>
-
 export const ClaudeLogo: React.ReactNode = <img
     src="/images/icons/claude-logo.png"
     alt="claude logo"
+    className="logo-image"
+/>
+
+export const CopilotLogo: React.ReactNode = <img
+    src="/images/icons/copilot-logo.png"
+    alt="copilot logo"
+    className="logo-image"
+/>
+
+export const D3Logo: React.ReactNode = <img
+    src="/images/icons/d3js-logo.png"
+    alt="d3js logo"
     className="logo-image"
 />
 
@@ -103,6 +70,18 @@ export const DremioLogo: React.ReactNode = <img
     className="logo-image"
 />
 
+export const EclipseLogo: React.ReactNode = <img
+    src="/images/icons/eclipse-logo.png"
+    alt="eclipse logo"
+    className="logo-image"
+/>
+
+export const ExcelLogo: React.ReactNode = <img
+    src="/images/icons/excel-logo.svg"
+    alt="excel logo"
+    className="logo-image"
+/>
+
 export const GitActionsLogo: React.ReactNode = <img
     src="/images/icons/git-actions-logo.png"
     alt="git actions logo"
@@ -115,10 +94,52 @@ export const GitLogo: React.ReactNode = <img
     className="logo-image"
 />
 
+export const GoLangLogo: React.ReactNode = <img
+    src="/images/icons/golang-logo.svg"
+    alt="python logo"
+    className="go-logo"
+/>
+
 export const GrafanaLogo: React.ReactNode = <img
     src="/images/icons/grafana-logo.png"
     alt="grafana logo"
     className="logo-image"
+/>
+
+export const GraphQLLogo: React.ReactNode = <img
+    src="/images/icons/graphql-logo.svg"
+    alt="graphQL logo"
+    className="logo-image"
+/>
+
+export const HerokuLogo: React.ReactNode = <img
+    src="/images/icons/heroku-logo.png"
+    alt="heroku logo"
+    className="logo-image"
+/>
+
+export const HTMLLogo: React.ReactNode = <img
+    src="/images/icons/html-logo.svg"
+    alt="html logo"
+    className="logo-image"
+/>
+
+export const IntellijLogo: React.ReactNode = <img
+    src="/images/icons/intellij-logo.png"
+    alt="intellij logo"
+    className="logo-image"
+/>
+
+export const JavaLogo: React.ReactNode = <img
+    src="/images/icons/java-logo.svg"
+    alt="javascript logo"
+    className="logo-image"
+/>
+
+export const JavascriptLogo: React.ReactNode = <img
+    src="/images/icons/javascript-logo.svg"
+    alt="javascript logo"
+    className="script-logo"
 />
 
 export const JiraLogo: React.ReactNode = <img
@@ -130,6 +151,12 @@ export const JiraLogo: React.ReactNode = <img
 export const KibanaLogo: React.ReactNode = <img
     src="/images/icons/kibana-logo.svg"
     alt="kibana logo"
+    className="logo-image"
+/>
+
+export const LinuxLogo: React.ReactNode = <img
+    src="/images/icons/linux-logo.png"
+    alt="linux logo"
     className="logo-image"
 />
 
@@ -157,6 +184,24 @@ export const OpsGenieLogo: React.ReactNode = <img
     className="logo-image"
 />
 
+export const OracleLogo: React.ReactNode = <img
+    src="/images/icons/oracle-logo.png"
+    alt="opsgenie logo"
+    className="logo-image"
+/>
+
+export const P5Logo: React.ReactNode  = <img
+    src="/images/icons/p5js-logo.svg"
+    alt="p5js logo"
+    className="logo-image"
+/>
+
+export const PoetryLogo: React.ReactNode = <img
+    src="/images/icons/poetry-logo.png"
+    alt="poetry logo"
+    className="logo-image"
+/>
+
 export const PostmanLogo: React.ReactNode = <img
     src="/images/icons/postman-logo.svg"
     alt="postman logo"
@@ -174,22 +219,71 @@ export const PuTTyLogo: React.ReactNode = <img
     alt="putty logo"
     className="logo-image"
 />
+
+export const PytestLogo: React.ReactNode = <img
+    src="/images/icons/pytest-logo.png"
+    alt="pytest logo"
+    className="logo-image"
+/>
+
+export const PythonLogo: React.ReactNode = <img
+    src="/images/icons/python-logo.svg"
+    alt="python logo"
+    className="logo-image"
+/>
+
 export const ReactLogo: React.ReactNode = <img
     src="/images/icons/react-logo.svg"
     alt="react logo"
     className="logo-image"
 />
+
+export const SonarQubeLogo: React.ReactNode = <img
+    src="/images/icons/sonarqube-logo.png"
+    alt="sonarqube logo"
+    className="sql-logo"
+/>
+
+export const SQLLogo: React.ReactNode = <img
+    src="/images/icons/sql-logo.svg"
+    alt="SQL logo"
+    className="sql-logo"
+/>
+
 export const TailwindLogo: React.ReactNode = <img
     src="/images/icons/tailwind-css-logo.png"
     alt="tailwind logo"
     className="logo-image"
 />
+
+export const TypescriptLogo: React.ReactNode = <img
+    src="/images/icons/typescript-logo.svg"
+    alt="typescript logo"
+    className="script-logo"
+/>
+
 export const VBALogo: React.ReactNode = <img
     src="/images/icons/vba-logo.png"
     alt="vba logo"
     className="logo-image"
 />
 
+export const VisualStudioLogo: React.ReactNode = <img
+    src="/images/icons/visual-studio-logo.svg"
+    alt="visual studio logo"
+    className="logo-image"
+/>
+
+export const CATEGORY: Record<string, string> = {
+    language: "language",
+    framework: "framework",
+    devtool: "devtool",
+    data: "data",
+    ide: "ide",
+    cloud: "cloud",
+    package: "package",
+    ai: "ai"
+}
 
 export type LogoItem = {
     readonly category: string;
@@ -199,256 +293,252 @@ export type LogoItem = {
 
 export const LOGO_MAP: Record<string, LogoItem> = {
     bash: {
-        category: "language",
+        category: CATEGORY.language,
         logo: BashLogo,
         name: "Bash"
     },
     cpp: {
-        category: "language",
+        category: CATEGORY.language,
         logo: CLogo,
         name: "C++"
     },
     css: {
-        category: "language",
+        category: CATEGORY.language,
         logo: CSSLogo,
         name: "CSS"
     },
     golang: {
-        category: "language",
+        category: CATEGORY.language,
         logo: GoLangLogo,
         name: "Golang"
     },
     graphql: {
-        category: "language",
+        category: CATEGORY.language,
         logo: GraphQLLogo,
         name: "Graphql"
     },
-
     html: {
-        category: "language",
+        category: CATEGORY.language,
         logo: HTMLLogo,
         name: "HTML5"
     },
     java: {
-        category: "language",
+        category: CATEGORY.language,
         logo: JavaLogo,
         name: "Java"
     },
     javascript: {
-        category: "language",
+        category: CATEGORY.language,
         logo: JavascriptLogo,
         name: "JavaScript"
     },
     python: {
-        category: "language",
+        category: CATEGORY.language,
         logo: PythonLogo,
         name: "Python"
     },
     sql: {
-        category: "language",
+        category: CATEGORY.language,
         logo: SQLLogo,
         name: "SQL"
     },
     typescirpt: {
-        category: "language",
+        category: CATEGORY.language,
         logo: TypescriptLogo,
         name: "TypeScript"
     },
-
     vba: {
-        category: "language",
+        category: CATEGORY.language,
         logo: VBALogo,
         name: "VBA"
     },
-    // d3js: {
-    //     logo: D3Logo,
-    //     name: "D3js",
-    //     category: "framework",
-    // },
+    d3js: {
+        logo: D3Logo,
+        name: "D3js",
+        category: CATEGORY.framework,
+    },
     nextjs: {
         logo: NextJsLogo,
         name: "Next.js",
-        category: "framework",
+        category: CATEGORY.framework,
     },
-    // p5js: {
-    //     logo: P5Logo,
-    //     name: "P5js",
-    //     category: "framework",
-    // },
+    p5js: {
+        logo: P5Logo,
+        name: "P5js",
+        category: CATEGORY.framework,
+    },
     react: {
         logo: ReactLogo,
         name: "React",
-        category: "framework",
+        category: CATEGORY.framework,
     },
     tailwind: {
         logo: TailwindLogo,
         name: "Tailwind",
-        category: "framework",
+        category: CATEGORY.framework,
     },
-    // pytest: {
-    //     logo: PytestLogo,
-    //     name: "Pytest",
-    //     category: "framework",
-    // },
-
+    pytest: {
+        logo: PytestLogo,
+        name: "Pytest",
+        category: CATEGORY.framework,
+    },
     
-    // // Data / Analytics Tools
+    // Data / Analytics Tools
     dremio: {
         logo: DremioLogo,
         name: "Dremio",
-        category: "devtool",
+        category: CATEGORY.devtool,
     },
     databricks: {
         logo: DbxLogo,
         name: "Databricks",
-        category: "devtool",
+        category: CATEGORY.devtool,
     },
-    // powerbi: {
-    //     logo: PowerBiLogo,
-    //     name: "Power BI",
-    //     category: "data",
-    // },
+    powerbi: {
+        logo: PowerBILogo,
+        name: "Power BI",
+        category: CATEGORY.data,
+    },
     grafana: {
         logo: GrafanaLogo,
         name: "Grafana",
-        category: "data",
+        category: CATEGORY.data,
     },
     kibana: {
         logo: KibanaLogo,
         name: "Kibana",
-        category: "data",
+        category: CATEGORY.data,
     },
-    // excel: {
-    //     logo: ExcelLogo,
-    //     name: "Excel",
-    //     category: "data",
-    // },
+    excel: {
+        logo: ExcelLogo,
+        name: "Excel",
+        category: CATEGORY.data,
+    },
     dbeaver: {
         logo: DBeaverLogo,
         name: "DBeaver",
-        category: "data",
+        category: CATEGORY.data,
     },
-    // oracle: {
-    //     logo: OracleLogo,
-    //     name: "Oracle",
-    //     category: "data",
-    // },
+    oracle: {
+        logo: OracleLogo,
+        name: "Oracle",
+        category: CATEGORY.data,
+    },
 
-    
     // IDEs
-    // intellij: {
-    //     logo: IntellijLogo,
-    //     name: "IntelliJ",
-    //     category: "ide",
-    // },
-    // eclipse: {
-    //     logo: EclipseLogo,
-    //     name: "Eclipse",
-    //     category: "ide",
-    // },
-    // visualStudio: {
-    //     logo: VisualStudioLogo,
-    //     name: "Visual Studio",
-    //     category: "ide",
-    // },
+    intellij: {
+        logo: IntellijLogo,
+        name: "IntelliJ",
+        category: CATEGORY.ide,
+    },
+    eclipse: {
+        logo: EclipseLogo,
+        name: "Eclipse",
+        category: CATEGORY.ide,
+    },
+    visualStudio: {
+        logo: VisualStudioLogo,
+        name: "Visual Studio",
+        category: CATEGORY.ide,
+    },
 
     // Developer Tools
     git: {
         logo: GitLogo,
         name: "Git",
-        category: "devtool",
+        category: CATEGORY.devtool,
     },
     github_actions: {
         logo: GitActionsLogo,
         name: "GitHub Actions",
-        category: "devtool",
+        category: CATEGORY.devtool,
     },
     docker: {
         logo: DockerLogo,
         name: "Docker",
-        category: "devtool",
+        category: CATEGORY.devtool,
     },
-    // linux: {
-    //     logo: LinuxLogo,
-    //     name: "Linux",
-    //     category: "devtool",
-    // },
+    linux: {
+        logo: LinuxLogo,
+        name: "Linux",
+        category: CATEGORY.devtool,
+    },
     postman: {
         logo: PostmanLogo,
         name: "Postman",
-        category: "devtool",
+        category: CATEGORY.devtool,
     },
     putty: {
         logo: PuTTyLogo,
         name: "PuTTY",
-        category: "devtool",
+        category: CATEGORY.devtool,
     },
-    // drawio: {
-    //     logo: DrawioLogo,
-    //     name: "Draw.io",
-    //     category: "devtool",
-    // },
+    drawio: {
+        logo: DrawIOLogo,
+        name: "Draw.io",
+        category: CATEGORY.devtool,
+    },
     mural: {
         logo: MuralLogo,
         name: "Mural",
-        category: "devtool",
+        category: CATEGORY.devtool,
     },
     opsgenie: {
         logo: OpsGenieLogo,
         name: "Opsgenie",
-        category: "devtool",
+        category: CATEGORY.devtool,
     },
-    // heroku: {
-    //     logo: HerokuLogo,
-    //     name: "Heroku",
-    //     category: "devtool",
-    // },
-
+    heroku: {
+        logo: HerokuLogo,
+        name: "Heroku",
+        category: CATEGORY.devtool,
+    },
     
     // Cloud
     aws: {
         logo: AWSLogo,
         name: "AWS",
-        category: "cloud",
+        category: CATEGORY.cloud,
     },
-
 
     // Package / Dependency Tools
     npm: {
         logo: NPMLogo,
         name: "npm",
-        category: "package",
+        category: CATEGORY.package,
     },
-    // poetry: {
-    //     logo: PoetryLogo,
-    //     name: "Poetry",
-    //     category: "package",
-    // },
-
-    
-
+    poetry: {
+        logo: PoetryLogo,
+        name: "Poetry",
+        category: CATEGORY.package,
+    },
 
     // AI / Code Assistance
     claude: {
         logo: ClaudeLogo,
         name: "Claude",
-        category: "ai",
+        category: CATEGORY.ai,
     },
-    // copilot: {
-    //     logo: SonarQubeLogo,
-    //     name: "SonarQube",
-    //     category: "ai",
-    // },
-    // sonarqube: {
-    //     logo: SonarQubeLogo,
-    //     name: "SonarQube",
-    //     category: "ai",
-    // },
+    copilot: {
+        logo: CopilotLogo,
+        name: "Copilot",
+        category: CATEGORY.ai,
+    },
+    sonarqube: {
+        logo: SonarQubeLogo,
+        name: "SonarQube",
+        category: CATEGORY.ai,
+    },
 };
+
+export type Tab = {
+    readonly category?: string;
+    readonly str?: string;
+}
 
 export type GridRow = {
     readonly rowContent: LogoItem[];
-    readonly tabs: Record<string, string | null>;
+    readonly tabs: Record<string, Tab | null>;
 }
 
 export const GRID_ROWS: Record<number, GridRow> = {
@@ -461,7 +551,10 @@ export const GRID_ROWS: Record<number, GridRow> = {
             LOGO_MAP.graphql,
         ],
         tabs: {
-            left: "Languages",
+            left: {
+                category: CATEGORY.language,
+                str: "Languages",
+            },
             right: null
         }
     },
@@ -484,40 +577,48 @@ export const GRID_ROWS: Record<number, GridRow> = {
             LOGO_MAP.nextjs,
             LOGO_MAP.react,
             LOGO_MAP.tailwind,
-            // LOGO_MAP.pytest,
-            // LOGO_MAP.d3js
+            LOGO_MAP.pytest,
+            LOGO_MAP.d3js
         ],
         tabs: {
             left: null,
-            right: "Frameworks",
+            right: {
+                category: CATEGORY.framework,
+                str: "Framworks",
+            }
         }
     },
     4: {
         rowContent: [
             LOGO_MAP.dremio,
             LOGO_MAP.databricks,
-            // LOGO_MAP.powerbi,
+            LOGO_MAP.powerbi,
             LOGO_MAP.grafana,
             LOGO_MAP.kibana,
-            // LOGO_MAP.excel,
+            LOGO_MAP.excel,
         ],
         tabs: {
-            left: "Data / Analytics",
+            left: {
+                category: CATEGORY.data,
+                str: "Data / Analytics",
+            },
             right: null,
         }
     },
     5: {
         rowContent: [
             LOGO_MAP.dbeaver,
-            // LOGO_MAP.oracle,
-            // LOGO_MAP.intellij,
-            // LOGO_MAP.eclipse,
-            // LOGO_MAP.visualStudio
-
+            LOGO_MAP.oracle,
+            LOGO_MAP.intellij,
+            LOGO_MAP.eclipse,
+            LOGO_MAP.visualStudio
         ],
         tabs: {
             left: null,
-            right: "IDEs",
+            right: {
+                category: CATEGORY.ide,
+                str: "IDEs",
+            },
         }
     },
     6: {
@@ -525,39 +626,51 @@ export const GRID_ROWS: Record<number, GridRow> = {
             LOGO_MAP.git,
             LOGO_MAP.github_actions,
             LOGO_MAP.docker,
-            // LOGO_MAP.linux,
+            LOGO_MAP.linux,
             LOGO_MAP.postman,
             LOGO_MAP.putty,
         ],
         tabs: {
-            left: "DevTools",
+            left: {
+                category: CATEGORY.devtool,
+                str: "Dev tools",
+            },
             right: null,
         }
     },
     7: {
         rowContent: [
-            // LOGO_MAP.drawio,
+            LOGO_MAP.drawio,
             LOGO_MAP.mural,
             LOGO_MAP.opsgenie,
-            // LOGO_MAP.heroku,
+            LOGO_MAP.heroku,
             LOGO_MAP.aws,
         ],
         tabs: {
             left: null,
-            right: "Cloud",
+            right: {
+                category: CATEGORY.cloud,
+                str: "Cloud",
+            },
         }
     },
     8: {
         rowContent: [
             LOGO_MAP.npm,
-            // LOGO_MAP.poetry,
+            LOGO_MAP.poetry,
             LOGO_MAP.claude,
-            // LOGO_MAP.copilot,
-            // LOGO_MAP.sonarqube,
+            LOGO_MAP.copilot,
+            LOGO_MAP.sonarqube,
         ],
         tabs: {
-            left: "Package / Dependency Tools",
-            right: "AI / Code Assistance",
+            left: {
+                category: CATEGORY.package,
+                str: "Package / Dependency tools",
+            },
+            right: {
+                category: CATEGORY.ai,
+                str: "AI / Code assistance",
+            },
         }
     }
 };
