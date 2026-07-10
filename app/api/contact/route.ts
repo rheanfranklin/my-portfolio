5878a79e02bc0976799cbd41e9bb713a411cbf74
rheanfranklin <rheannon.franklin@gmail.com>
2026-07-10 19:01:48 -0400

    commenting out entire function

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -35,7 +35,7 @@ function rateLimit(ip: string) {
 // }
 
 export async function POST(req: Request) {
-  const resend: Resend = new Resend(process.env.RESEND_API_KEY);
+  // const resend: Resend = new Resend(process.env.RESEND_API_KEY);
   // const ip =
   //   req.headers.get("x-forwarded-for") ||
   //   req.headers.get("x-real-ip") ||
