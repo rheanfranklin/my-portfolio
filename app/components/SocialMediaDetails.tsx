8bb92d9a2e9bf39bec61e4ebfafe5946da690f84
rheanfranklin <rheannon.franklin@gmail.com>
2026-07-18 21:26:47 -0400

    fix: shifting socials icons

--- app/components/SocialMediaDetails.tsx
+++ app/components/SocialMediaDetails.tsx
@@ -6,12 +6,14 @@ export const GitLogoPixeled: React.ReactNode = <img
     src="/images/icons/git-logo-pixeled.png"
     alt="pixeled git logo"
     className="icon"
+    style={{ marginTop: '2px' }}
 />
 
 export const LinkedinPixeled: React.ReactNode = <img
     src="/images/icons/linkedin-logo-pixeled.png"
     alt="pixeled linkedin logo"
     className="icon"
+    style={{ marginTop: '2px' }}
 />
 
 export const SocialMediaDetails = () => {
